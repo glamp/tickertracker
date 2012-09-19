e17bf1dcb5680e2ef00c928528ab2e9b5ef4abc7
Ubuntu <ubuntu@ip-10-117-85-115.ec2.internal>
2012-09-19 01:42:04 +0000

    switched to csv

--- stocks.py
+++ stocks.py
@@ -1,5 +1,6 @@
 import urllib2, urllib
 import json
+import csv
 import pprint as pp
 import random
 import time
@@ -34,7 +35,10 @@ stocks = ["'" + stock + "'" for stock in stocks]
 
 random.shuffle(stocks)
 
-f = open('/home/ubuntu/repo/flatfiles/stockdata.json', 'wb')
+f = open('/home/ubuntu/repo/flatfiles/stockdata.csv', 'wb')
+w = csv.writer(f)
+columns = [u'AfterHoursChangeRealtime', u'AnnualizedGain', u'Ask', u'AskRealtime', u'AverageDailyVolume', u'Bid', u'BidRealtime', u'BookValue', u'Change', u'ChangeFromFiftydayMovingAverage', u'ChangeFromTwoHundreddayMovingAverage', u'ChangeFromYearHigh', u'ChangeFromYearLow', u'ChangePercentRealtime', u'ChangeRealtime', u'Change_PercentChange', u'ChangeinPercent', u'Commission', u'DaysHigh', u'DaysLow', u'DaysRange', u'DaysRangeRealtime', u'DaysValueChange', u'DaysValueChangeRealtime', u'DividendPayDate', u'DividendShare', u'DividendYield', u'EBITDA', u'EPSEstimateCurrentYear', u'EPSEstimateNextQuarter', u'EPSEstimateNextYear', u'EarningsShare', u'ErrorIndicationreturnedforsymbolchangedinvalid', u'ExDividendDate', u'FiftydayMovingAverage', u'HighLimit', u'HoldingsGain', u'HoldingsGainPercent', u'HoldingsGainPercentRealtime', u'HoldingsGainRealtime', u'HoldingsValue', u'HoldingsValueRealtime', u'LastTradeDate', u'LastTradePriceOnly', u'LastTradeRealtimeWithTime', u'LastTradeTime', u'LastTradeWithTime', u'LowLimit', u'MarketCapRealtime', u'MarketCapitalization', u'MoreInfo', u'Name', u'Notes', u'OneyrTargetPrice', u'Open', u'OrderBookRealtime', u'PEGRatio', u'PERatio', u'PERatioRealtime', u'PercebtChangeFromYearHigh', u'PercentChange', u'PercentChangeFromFiftydayMovingAverage', u'PercentChangeFromTwoHundreddayMovingAverage', u'PercentChangeFromYearLow', u'PreviousClose', u'PriceBook', u'PriceEPSEstimateCurrentYear', u'PriceEPSEstimateNextYear', u'PricePaid', u'PriceSales', u'SharesOwned', u'ShortRatio', u'StockExchange', u'Symbol', u'TickerTrend', u'TradeDate', u'TwoHundreddayMovingAverage', u'Volume', u'YearHigh', u'YearLow', u'YearRange', 'datestamp', u'symbol', 'timestamp']
+w.writerow(columns)
 
 for block in range(0, len(stocks), 150):
     stocks_subset = stocks[block:block+150]
@@ -65,7 +69,7 @@ for block in range(0, len(stocks), 150):
         quote['timestamp'] = int(time.time())
         quote['datestamp'] = str(datetime.now())
         pp.pprint(quote)
-        f.write(json.dumps(quote) + '\n')
+        w.writerow([quote.get(col) for col in columns]
         print "*"*80
 
 f.close()
